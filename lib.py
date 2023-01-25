2a1dbc1e9afd9d5614e2801ac8983c2978c3fe09
Sebastian Seitz <sebastian.seitz@mediaman.de>
2023-01-25 20:47:46 +0100

    add backend

--- lib.py
+++ lib.py
@@ -9,6 +9,8 @@ from dash_extensions.enrich import callback
 from plotly_resampler import FigureResampler
 from trace_updater import TraceUpdater
 
+redis_backend = RedisStore(host='127.0.0.1', port='6379')
+
 
 def GraphWithTraceUpdater(graph_id):
     graph = dcc.Graph(id=graph_id)
@@ -16,8 +18,7 @@ def GraphWithTraceUpdater(graph_id):
     trace = TraceUpdater(gdID=graph_id)
 
     @callback(
-        ServersideOutput(store, 'data', backend=RedisStore(
-            host='127.0.0.1', port='6379')),
+        ServersideOutput(store, 'data', backend=redis_backend),
         Input(graph, 'figure'),
         prevent_initial_call=True,
     )
